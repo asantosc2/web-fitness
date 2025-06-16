c9547e3635562ef40bc856ed76f435c2528d6ff5
Alejandro Santos Cabrera <a.santosc02@gmail.com>
2025-06-16 05:08:27 +0200

    feat: eliminar valor predeterminado de DATABASE_URL para usar solo variables de entorno

--- web_fitness_back/app/db.py
+++ web_fitness_back/app/db.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/web_fitness")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 
