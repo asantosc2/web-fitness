c7fe41e9d60ca0a39d32ac82ca8ba43ecdb381d2
Alejandro Santos Cabrera <a.santosc02@gmail.com>
2025-06-16 05:01:59 +0200

    feat: actualizar la configuración de la URL de la base de datos para usar variables de entorno

--- web_fitness_back/app/db.py
+++ web_fitness_back/app/db.py
@@ -1,6 +1,7 @@
 from sqlmodel import create_engine, Session
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/web_fitness"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/web_fitness")
 engine = create_engine(DATABASE_URL, echo=True)
 
 
